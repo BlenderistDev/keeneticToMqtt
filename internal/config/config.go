4e273f38541733bc6794558facb72e9d9bde7208
BlenderistDev <demonx.blenderist@gmail.com>
2024-08-14 00:04:55 +0300

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,72 @@
+package configs
+
+import (
+	"os"
+	"strings"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+var conFile string
+
+type Config struct {
+	Keenetic      Keenetic      `mapstructure:"keenetic"`
+	Mqtt          Mqtt          `mapstructure:"mqtt"`
+	Homeassistant HomeAssistant `mapstructure:"homeassistant"`
+}
+
+type Keenetic struct {
+	Host     string `mapstructure:"host"`
+	Login    string `mapstructure:"login"`
+	Password string `mapstructure:"password"`
+}
+
+type Mqtt struct {
+	Host      string `mapstructure:"host"`
+	Login     string `mapstructure:"login"`
+	Password  string `mapstructure:"password"`
+	ClientID  string `mapstructure:"clientId"`
+	BaseTopic string `mapstructure:"baseTopic"`
+}
+
+type HomeAssistant struct {
+	UpdateInterval time.Duration `mapstructure:"updateInterval"`
+}
+
+func SetConfigFile(path string) {
+	conFile = path
+}
+
+func NewDefaultConfig() (*Config, error) {
+	if err := InitializeConfig(); err != nil {
+		return nil, err
+	}
+	config := Config{}
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func InitializeConfig() error {
+	path, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	if conFile != "" {
+		viper.SetConfigFile(strings.TrimSpace(conFile))
+	} else {
+		viper.SetConfigFile(strings.TrimSpace(path + "/configs/config.yml"))
+	}
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return err
+	}
+	return nil
+}
